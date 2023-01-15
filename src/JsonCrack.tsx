fe74002ffea932497f8c22c9bb52cad1b7760c13
Suni <yysuni1001@gmail.com>
2023-01-16 01:16:41 +0800

    Add reaflow

--- src/JsonCrack.tsx
+++ src/JsonCrack.tsx
@@ -1,3 +1,27 @@
-export default function JsonCrack() {
-  return <div>Json Crack</div>;
+import { Canvas, MarkerArrow, CanvasContainerProps } from 'reaflow'
+
+export default function JsonCrack(props: CanvasContainerProps) {
+	return (
+		<Canvas
+			{...props}
+			arrow={<MarkerArrow />}
+			nodes={[
+				{
+					id: '1',
+					text: '1'
+				},
+				{
+					id: '2',
+					text: '2'
+				}
+			]}
+			edges={[
+				{
+					id: '1-2',
+					from: '1',
+					to: '2'
+				}
+			]}
+		/>
+	)
 }
